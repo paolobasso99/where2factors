c821eab11707417ee8ea38045765efd7e6bc9732
Paolo Basso <basso.paolo99@gmail.com>
2020-08-30 00:14:00 +0200

    clear

--- loaders/index.js
+++ loaders/index.js
@@ -1,8 +1,11 @@
 const expressLoader = require('./expressLoader');
+const mongooseLoader = require('./mongooseLoader');
 const routesLoader = require('./routesLoader');
 
 module.exports = (app) => {
   expressLoader(app);
+
+  mongooseLoader(app);
 
   routesLoader(app);
 
