de7bc6163b020447c6fca24023003dc52696c532
Paolo Basso <basso.paolo99@gmail.com>
2020-10-04 13:39:19 +0200

    Broke tests

--- test/WebsitesUpdater.test.js
+++ test/WebsitesUpdater.test.js
@@ -1,36 +1,20 @@
 process.env.NODE_ENV = 'test';
 const { resolve } = require('path');
 require('dotenv').config({ path: resolve(__dirname, '../.env.test') });
-
 const { assert } = require('chai');
 const sinon = require('sinon');
 const axios = require('axios');
 const fs = require('fs');
+const { after } = require('mocha');
+
 const connectDB = require('../db/connectDB');
 const disconnectDB = require('../db/disconnectDB');
 const WebsitesUpdater = require('../components/websites/WebsitesUpdater');
 const WebsitesDB = require('../components/websites/WebsitesDB');
-const { after } = require('mocha');
-
-const website = {
-  name: 'Example',
-  url: 'https://www.example.com',
-  host: 'www.example.com',
-  category: 'example',
-  domain: 'example.com',
-  twitter: 'example',
-  facebook: 'example',
-  email_address: 'example',
-  img: 'example.png',
-  doc: 'https://example.com/doc',
-  tfa: ['method1', 'method2'],
-  exception: 'example',
-  status: 'example',
-};
 
 describe('WebsitesUpdater', function () {
   describe('get websites from github', function () {
-    after(function () {
+    afterEach(function () {
       sinon.restore();
     });
 
@@ -46,7 +30,15 @@ describe('WebsitesUpdater', function () {
       const ymlUrls = await WebsitesUpdater.getCategoriesYmlUrls();
 
       assert.isArray(ymlUrls);
-      assert.isNotEmpty(ymlUrls);
+      assert.lengthOf(ymlUrls, 2);
+      assert.equal(
+        ymlUrls[0],
+        'https://raw.githubusercontent.com/2factorauth/twofactorauth/master/_data/backup.yml'
+      );
+      assert.equal(
+        ymlUrls[1],
+        'https://raw.githubusercontent.com/2factorauth/twofactorauth/master/_data/banking.yml'
+      );
     });
 
     it('getWebsitesFromYmlUrl(ymlUrl) should return a non empty array', async function () {
@@ -61,40 +53,67 @@ describe('WebsitesUpdater', function () {
       );
 
       assert.isArray(websites);
-      assert.isNotEmpty(websites);
+      assert.lengthOf(websites, 2);
+      await assert.hasAllDeepKeys(websites[0], ['name', 'url', 'img', 'tfa', 'category']);
+      await assert.hasAllDeepKeys(websites[1], ['name', 'url', 'img', 'tfa', 'category']);
     });
 
     it('getWebsites() should return a non empty array', async function () {
+      // Stub
+      sinon
+        .stub(WebsitesUpdater, 'getCategoriesYmlUrls')
+        .resolves([
+          'https://raw.githubusercontent.com/2factorauth/twofactorauth/master/_data/backup.yml',
+        ]);
+      const response = {
+        data: fs.readFileSync('./test/twofactorauth.org/backup.yml'),
+      };
+      sinon.stub(axios, 'get').resolves(response);
+
       const websites = await WebsitesUpdater.getWebsites();
 
       assert.isArray(websites);
-      assert.isNotEmpty(websites);
+      assert.lengthOf(websites, 2);
+      await assert.hasAllDeepKeys(websites[0], ['name', 'url', 'img', 'tfa', 'category']);
+      await assert.hasAllDeepKeys(websites[1], ['name', 'url', 'img', 'tfa', 'category']);
     });
   });
 
   describe('update', function () {
-    before(async function () {
-      await connectDB();
-      await WebsitesDB.deleteAll();
-      await disconnectDB();
-    });
-
     afterEach(async function () {
-      await disconnectDB();
       sinon.restore();
     });
 
     it('should add websites to the database', async function () {
-      sinon.stub(WebsitesUpdater, 'getWebsites').resolves([website]);
+      // Stub getWebsites
+      const mockWebsites = [
+        {
+          name: 'Example',
+          url: 'https://www.example.com',
+          category: 'example',
+          img: 'example.png',
+          tfa: ['method1', 'method2'],
+        },
+        {
+          name: 'Example2',
+          url: 'https://www.example2.com',
+          category: 'example2',
+          img: 'example2.png',
+          tfa: ['method1', 'method2'],
+        },
+      ];
+      sinon.stub(WebsitesUpdater, 'getWebsites').resolves([mockWebsites]);
+      sinon.stub(WebsitesDB, 'addOrUpdate').resolves();
+
+      // Stub database
+      sinon.stub(connectDB);
+      sinon.stub(disconnectDB);
 
       // Update
       await WebsitesUpdater.update();
 
-      // Check database
-      await connectDB();
-      const check = await WebsitesDB.findByHost('www.example.com');
-
-      await assert.deepNestedInclude(check, website);
+      // Check database calls
+      assert.equal(WebsitesDB.addOrUpdate.callCount, 2);
     });
   });
 });
